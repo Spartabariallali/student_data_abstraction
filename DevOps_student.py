4461f95903913f55be3faccfcafb9e8e76bd5a9b
MOH <iAllali@Baris-macbook-pro.local>
2020-07-06 09:17:27 +0100

    student abstraction

---
+++ DevOps_student.py
@@ -0,0 +1,2 @@
+from student_data import Student_name
+
---
+++ student_data.py
@@ -0,0 +1,6 @@
+class Student_name:
+    def first_last_name(self,first,last):
+        first = str(input("please enter your first name: "))
+        last = str(input("please enter your last name: "))
+        print(first + last)
+
