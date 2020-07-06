8b106dd85f0453af6d791e722e607b05fe911bc4
Spartabariallali <67052846+Spartabariallali@users.noreply.github.com>
2020-07-06 11:01:36 +0100

    Update student_data.py

--- student_data.py
+++ student_data.py
@@ -1,6 +1,14 @@
 class Student_name:
-    def first_last_name(self,first,last):
-        first = str(input("please enter your first name: "))
-        last = str(input("please enter your last name: "))
-        print(first + last)
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def full_name(self):
+        print(self.first_name, " ", self.last_name)
+
+
+student_one = Student_name("bari","allali")
+
+student_one.full_name()
+student_one.roll_call()
 
